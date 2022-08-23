94091d405667359054c1460d29d58674dbd4ac83
prkhr <prkhr1121@gmail.com>
2022-08-23 11:08:03 +0530

    added cart, registration, login

--- server.js
+++ server.js
@@ -4,15 +4,24 @@ const ejs = require('ejs')
 const expressLayout = require('express-ejs-layouts')
 const path = require('path')
 const PORT = process.env.PORT || 3300
-app.get('/', (req, res) => {
-    res.render('home')
-})
 //Assets
 app.use(express.static('public'))
 // set template engine
 app.use(expressLayout)
 app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
+app.get('/', (req, res) => {
+    res.render('home')
+})
+app.get('/cart', (req, res) => {
+    res.render('customers/cart');
+})
+app.get('/login', (req, res) => {
+    res.render('auth/login');
+})
+app.get('/register', (req, res) => {
+    res.render('auth/register');
+})
 app.listen(PORT, () => {
     console.log(`listning on port ${PORT}`)
 })
